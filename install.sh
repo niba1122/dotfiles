a2e6c80da32553af246a4b33a22f6376bb6b3610
Nobuhito Ibaraki <niba1122@gmail.com>
2015-08-06 01:03:29 +0900

    うまくいかない存在判定を一旦削除

--- install.sh
+++ install.sh
@@ -2,19 +2,6 @@
 
 DOTFILES_PATH=$HOME'/.dotfiles'
 
-if [ -d ~/.oh-my-zsh ]; then
-  mv -R ~/.oh-my-zsh ~/_oh-my-zsh
-fi
-if [ -a ~/.zshrc ]; then
-  mv ~/.zshrc ~/_zshrc
-fi
-if [ -a ~/.vimrc ]; then
-  mv ~/.vimrc ~/_vimrc
-fi
-if [ -a ~/.vim ]; then
-  mv -R ~/.vim ~/_vim
-fi
-
 ln -nfs $DOTFILES_PATH/.oh-my-zsh/ $HOME/.oh-my-zsh
 ln -nfs $DOTFILES_PATH/.zshrc $HOME/.zshrc
 ln -nfs $DOTFILES_PATH/.vimrc $HOME/.vimrc
