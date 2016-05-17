c0957349022c1c43f49675a6a0d95a90a7ac5d49
Nobuhito Ibaraki <niba1122@gmail.com>
2016-05-18 01:17:45 +0900

    karabinerのキーバインド設定ファイルを追加

---
+++ karabiner/keybind.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set option.emacsmode_controlY 1
+/bin/echo -n .
+$cli set option.emacsmode_optionV 1
+/bin/echo -n .
+$cli set option.emacsmode_optionBF 1
+/bin/echo -n .
+$cli set option.emacsmode_controlH 1
+/bin/echo -n .
+$cli set option.emacsmode_controlV 1
+/bin/echo -n .
+$cli set option.emacsmode_controlPNBF_nomodifiers 1
+/bin/echo -n .
+$cli set option.emacsmode_optionLtGt 1
+/bin/echo -n .
+$cli set option.emacsmode_controlK 1
+/bin/echo -n .
+$cli set option.emacsmode_controlD_nomodifiers 1
+/bin/echo -n .
+/bin/echo
