0819c932b862e18dbf0b48bcd1af4ca065c0e82d
Nobuhito Ibaraki <niba1122@gmail.com>
2016-05-18 02:43:20 +0900

    karabinerとtmuxの設定を変更

--- karabiner/keybind.sh
+++ karabiner/keybind.sh
@@ -2,22 +2,28 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set option.emacsmode_controlV 1
+/bin/echo -n .
+$cli set repeat.initial_wait 300
+/bin/echo -n .
+$cli set option.emacsmode_controlD_nomodifiers 1
+/bin/echo -n .
+$cli set remap.jis_commandL2commandL_toggle_kana_eisuu 1
+/bin/echo -n .
 $cli set option.emacsmode_controlY 1
 /bin/echo -n .
 $cli set option.emacsmode_optionV 1
+/bin/echo -n .
+$cli set option.emacsmode_optionLtGt 1
 /bin/echo -n .
 $cli set option.emacsmode_optionBF 1
 /bin/echo -n .
 $cli set option.emacsmode_controlH 1
 /bin/echo -n .
-$cli set option.emacsmode_controlV 1
-/bin/echo -n .
-$cli set option.emacsmode_controlPNBF_nomodifiers 1
-/bin/echo -n .
-$cli set option.emacsmode_optionLtGt 1
+$cli set repeat.wait 10
 /bin/echo -n .
 $cli set option.emacsmode_controlK 1
 /bin/echo -n .
-$cli set option.emacsmode_controlD_nomodifiers 1
+$cli set option.emacsmode_controlPNBF 1
 /bin/echo -n .
 /bin/echo
