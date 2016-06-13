173e7afe27fe189c4675318197061fe4a748a84c
Nobuhito Ibaraki <niba1122@gmail.com>
2016-06-14 01:16:30 +0900

    日本語の切り替えを右commandに変更

--- karabiner/keybind.sh
+++ karabiner/keybind.sh
@@ -2,28 +2,30 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set option.emacsmode_controlV 1
-/bin/echo -n .
-$cli set remap.jis_commandL2commandL_toggle_kana_eisuu 1
-/bin/echo -n .
 $cli set option.emacsmode_controlD_nomodifiers 1
-/bin/echo -n .
-$cli set option.emacsmode_controlY 1
-/bin/echo -n .
-$cli set option.emacsmode_optionV 1
-/bin/echo -n .
-$cli set option.emacsmode_optionLtGt 1
-/bin/echo -n .
-$cli set option.emacsmode_controlW 1
-/bin/echo -n .
-$cli set option.emacsmode_optionBF 1
 /bin/echo -n .
 $cli set option.emacsmode_controlH 1
 /bin/echo -n .
 $cli set option.emacsmode_controlK 1
 /bin/echo -n .
+$cli set option.emacsmode_controlPNBF 1
+/bin/echo -n .
+$cli set option.emacsmode_controlV 1
+/bin/echo -n .
+$cli set option.emacsmode_controlW 1
+/bin/echo -n .
+$cli set option.emacsmode_controlY 1
+/bin/echo -n .
+$cli set option.emacsmode_optionBF 1
+/bin/echo -n .
+$cli set option.emacsmode_optionLtGt 1
+/bin/echo -n .
+$cli set option.emacsmode_optionV 1
+/bin/echo -n .
 $cli set option.emacsmode_OptionWCopy 1
 /bin/echo -n .
-$cli set option.emacsmode_controlPNBF 1
+$cli set remap.jis_commandR2commandR_toggle_kana_eisuu 1
+/bin/echo -n .
+$cli set repeat.initial_wait 250
 /bin/echo -n .
 /bin/echo
