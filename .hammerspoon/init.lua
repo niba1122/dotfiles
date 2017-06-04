3458a12cd20a8c31d9be0214e4ddbf60b7ff8847
Nobuhito Ibaraki <niba1122@gmail.com>
2017-06-05 03:11:09 +0900

    コマンドキーで英数切り替え

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -66,3 +66,44 @@ remapKey({'ctrl'}, 'v', keyCode('pagedown'))
 remapKey({'alt'}, 'v', keyCode('pageup'))
 remapKey({'cmd', 'shift'}, ',', keyCode('home'))
 remapKey({'cmd', 'shift'}, '.', keyCode('end'))
+
+-- from http://mizoguche.info/2017/01/hammerspoon_for_sierra/
+
+local prevKeyCode
+local escape = 0x35
+local leftCommand = 0x37
+local rightCommand = 0x36
+local eisuu = 0x66
+local kana = 0x68
+
+local function keyStroke(modifiers, character)
+    hs.eventtap.keyStroke(modifiers, character)
+end
+
+local function jp()
+    keyStroke({}, kana)
+end
+
+local function eng()
+    keyStroke({}, eisuu)
+end
+
+local function handleEvent(e)
+    local keyCode = e:getKeyCode()
+    if keyCode == escape then
+        eng()
+    end
+
+
+    local isCmdKeyUp = not(e:getFlags()['cmd']) and e:getType() == hs.eventtap.event.types.flagsChanged
+    if isCmdKeyUp and prevKeyCode == leftCommand then
+        eng()
+    elseif isCmdKeyUp and prevKeyCode == rightCommand then
+        jp()
+    end
+
+    prevKeyCode = keyCode
+end
+
+eventtap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged, hs.eventtap.event.types.keyDown, hs.eventtap.event.types.keyUp}, handleEvent)
+eventtap:start()
